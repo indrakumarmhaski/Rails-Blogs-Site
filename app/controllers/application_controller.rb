b05bbc93ce04c7114feaa8a965dc2314249bf2d5
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-06-08 19:30:23 +0530

    Added about field for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name, :about])
 
     end
 end
