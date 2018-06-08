d9a7d0f1c257b76bc63b559b61fd8a80f1cbf46b
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-06-08 17:31:54 +0530

    Added name to users and completed authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_081913) do
+ActiveRecord::Schema.define(version: 2018_06_08_114353) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_06_08_081913) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
